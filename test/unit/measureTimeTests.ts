e0c049682b402db56e0b62ac63123d2de18732d6
Noah Hummel <strangedev@posteo.net>
2021-08-24 09:40:21 +0200

    fix: await timeouts

--- test/unit/measureTimeTests.ts
+++ test/unit/measureTimeTests.ts
@@ -13,35 +13,47 @@ suite('measureTime', (): void => {
   test('measures short time ranges.', async (): Promise<void> => {
     const getElapsed = measureTime();
 
-    setTimeout((): void => {
-      const elapsed = getElapsed();
+    await new Promise<void>((resolve): void => {
+      setTimeout((): void => {
+        const elapsed = getElapsed();
 
-      assert.that(elapsed.seconds).is.equalTo(0);
-      assert.that(elapsed.milliseconds).is.atLeast(100);
-      assert.that(elapsed.milliseconds).is.lessThan(125);
-    }, 100);
+        assert.that(elapsed.seconds).is.equalTo(0);
+        assert.that(elapsed.milliseconds).is.atLeast(100);
+        assert.that(elapsed.milliseconds).is.lessThan(125);
+
+        resolve();
+      }, 100);
+    });
   });
 
   test('measures longer time ranges.', async (): Promise<void> => {
     const getElapsed = measureTime();
 
-    setTimeout((): void => {
-      const elapsed = getElapsed();
+    await new Promise<void>((resolve): void => {
+      setTimeout((): void => {
+        const elapsed = getElapsed();
 
-      assert.that(elapsed.seconds).is.equalTo(1);
-      assert.that(elapsed.milliseconds).is.atLeast(500);
-      assert.that(elapsed.milliseconds).is.lessThan(525);
-    }, 1_500);
+        assert.that(elapsed.seconds).is.equalTo(1);
+        assert.that(elapsed.milliseconds).is.atLeast(500);
+        assert.that(elapsed.milliseconds).is.lessThan(525);
+
+        resolve();
+      }, 1_500);
+    });
   });
 
   test('calculates the total milliseconds.', async (): Promise<void> => {
     const getElapsed = measureTime();
 
-    setTimeout((): void => {
-      const elapsed = getElapsed();
+    await new Promise<void>((resolve): void => {
+      setTimeout((): void => {
+        const elapsed = getElapsed();
 
-      assert.that(elapsed.millisecondsTotal).is.atLeast(1_500);
-      assert.that(elapsed.millisecondsTotal).is.lessThan(1_525);
-    }, 1_500);
+        assert.that(elapsed.millisecondsTotal).is.atLeast(1_500);
+        assert.that(elapsed.millisecondsTotal).is.lessThan(1_525);
+
+        resolve();
+      }, 1_500);
+    });
   });
 });
